9e0fab365ef52b42bfbb897699a0d0e597152e7f
dongjay00 <cjswodlehdgn@gmail.com>
2025-10-28 13:35:35 +0900

    fix: default color

--- src/lib/store/themeStore.ts
+++ src/lib/store/themeStore.ts
@@ -9,7 +9,7 @@ interface ThemeState {
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
-      isDarkMode: false,
+      isDarkMode: true,
       toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
     }),
     {
